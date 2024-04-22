ce755ef2850afe2826f1abb996915345475bc03f
Viraj Gavade <165892389+viraj-gavade@users.noreply.github.com>
2024-04-22 15:08:54 +0530

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const url = "https://newsapi.org/v2/everything?q=";
 const apiKey = "7b993c66c7a44882b08579799d10bc6c";
+const searchtext = document.getElementById("search-text")
+const searchbutton=document.getElementById("search-button")
+const headline=document.getElementById("headline")
+
+
+
 
 const fetchNews = async (query) => {
+  headline.innerHTML=`<h1 class="headline">NEWS / ${query}</h1>`
+  let container = document.getElementById("container");
+  container.innerHTML = `<h2>Fetching News Related ${query}...</h2>`; // Clear previous content
   try {
     let response = await fetch(`${url}${query}&apiKey=${apiKey}`);
     let data = await response.json();
     bindData(data.articles);
   } catch (error) {
-    console.error("Error fetching news:", error);
+    console.error("Error fetching news...", error);
   }
 };
+searchbutton.addEventListener('click',()=>{
+  const searchtxt = searchtext.value.trim()
+   fetchNews(searchtxt) 
+})
 
 const bindData = (articles) => {
   let container = document.getElementById("container");
@@ -50,13 +63,4 @@ function Navitemactive(id) {
   currentSelectedNav = navItem;
   currentSelectedNav.classList.add("active");
 }
-
-const searchbutton = document.getElementById("search-button")
-const searchtext = document.getElementById("search-text")
-
-searchbutton.addEventListener("click",()=>{
-  const query = searchtext.value
-  if(!query)return;
-  fetchNews(query)
-})
 window.onload(fetchNews('top'))
